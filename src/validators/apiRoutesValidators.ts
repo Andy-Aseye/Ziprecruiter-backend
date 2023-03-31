5c8a89eab3ecd80a25064a06602f09b6687f7deb
wevs <68214494+dopewevmond@users.noreply.github.com>
2023-03-31 07:49:37 +0000

    yup validation for api routes

---
+++ src/validators/apiRoutesValidators.ts
@@ -0,0 +1,52 @@
+import * as yup from 'yup'
+
+export const postJobSchema = yup.object({
+  body: yup.object({
+    title: yup.string().required(),
+    description: yup.string().required(),
+    skills: yup.string().required(),
+    jobType: yup.string().required(),
+    duration: yup.string().required(),
+    salary: yup.string().required(),
+  })
+})
+
+export const editJobSchema = yup.object({
+  body: yup.object({
+    salary: yup.string().notRequired(),
+    duration: yup.string().notRequired(),
+    deadline: yup.string().notRequired()
+  }),
+  params: yup.object({
+    id: yup.string().required()
+  })
+})
+
+export const idParamSchema = yup.object({
+  params: yup.object({
+    id: yup.string().required()
+  })
+})
+
+export const editUserSchema = yup.object({
+  body: yup.object({
+    name: yup.string().notRequired(),
+    organization: yup.string().notRequired(),
+    position: yup.string().notRequired(),
+    contactNumber: yup.string().notRequired(),
+    education: yup.string().notRequired(),
+    skills: yup.string().notRequired(),
+    resume: yup.string().notRequired(),
+    profile: yup.string().notRequired(),
+  })
+})
+
+export const editApplicationSchema = yup.object({
+  body: yup.object({
+    status: yup.string().notRequired(),
+    dateOfJoining: yup.string().notRequired(),
+  }),
+  params: yup.object({
+    id: yup.string().required()
+  })
+})
